f7b84d4e2b75dc5836f2157f5cd37157995f94de
Kim Ngân <Kim Ngn>
2017-04-01 16:32:29 +0700

    layout

--- resources/views/auth/feedback.blade.php
+++ resources/views/auth/feedback.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <div class="content-feedback">
         <section class="main-content col-lg-8 col-md-8 col-sm-8">
-            <form method="post" action="{{url('register')}}">
+            <form method="post" action="{{url('feedback')}}">
                 <div class="row">
                     <div class="col-lg-12 col-md-12 col-sm-12 register-account">
                         <div class="green">
@@ -17,15 +17,11 @@
                         </div>
                         <div class="page-content">
                             <div class="body-feedback">
-                                <p>Email</p>
-                                <hr>
                                 <div class="input-group">
                                     <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
                                     <input type="email" id="Home_ContentPlaceHolder_tb_Email" placeholder="E-mail" class="form-control" name="email" required>
                                     <i class="icons icon-user-3"></i>
                                 </div>
-                                <p>Feedback</p>
-                                <hr>
                                 <div class="input-feedback">
                                   <textarea type="text" class="form-control" name="msg" placeholder="Add feedback" required></textarea>
                                 </div>
@@ -61,6 +57,11 @@
                             </div>
                         </div>
                         <div class="row">
+                            <div class="blue">
+                                <div class="carousel-heading no-margin">
+                                    <h4>Đăng ký tài khoản</h4>
+                                </div>
+                            </div>
                             <div class="col-md-12 col-sm-12 col-xs-12">
                                 <p>Nếu bạn chưa có tài khoản</p>
                                 <div class="product-actions-single">
