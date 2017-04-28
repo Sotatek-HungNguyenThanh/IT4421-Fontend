fa705e4290ec64b3953c04fe37c3101ce42af930
Hung Nguyen <hungnguyen.vp.1995@gmail.com>
2017-04-28 14:45:33 +0700

    change host api

--- app/Consts.php
+++ app/Consts.php
@@ -11,6 +11,5 @@ namespace App;
 
 class Consts
 {
-//    const HOST = "http://52.38.245.99/api/v1/";
-    const HOST = "http://3c158e1c.ngrok.io/api/v1/";
+    const HOST = "http://52.38.245.99/api/v1/";
 }
