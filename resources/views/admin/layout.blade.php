b357d535ccd6e8aaab4c45a6a54f06a1054feef4
Hung Nguyen <hungnguyen.vp.1995@gmail.com>
2017-05-28 07:04:06 +0700

    fix

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -28,9 +28,7 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.min.js"></script>
     <script type="text/javascript" src="/js/angular/core/BaseClass.js"></script>
-    <script type="text/javascript" src="/js/angular/core/Filter.js"></script>
     <script type="text/javascript" src="/js/angular/admin/AdminFilter.js"></script>
-    <script type="text/javascript" src="/js/angular/core/Service.js"></script>
     <script type="text/javascript" src="/js/angular/admin/AdminService.js"></script>
     <script type="text/javascript" src="/js/angular/core/component.js"></script>
 
